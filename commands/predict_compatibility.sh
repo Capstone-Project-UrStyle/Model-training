80ad79ee68de523b1d08a5a2d4c95fc3f9c2fd2f
Nguyễn Đặng Tuấn Kiệt <tuankietnk2001@gmail.com>
2023-04-09 11:29:08 +0700

    Adding predict outfit compatibility function and command

---
+++ commands/predict_compatibility.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CHECKPOINT_DIR="models/model_final/model.ckpt-34865"
+
+python src/fashion_compatibility.py \
+  --checkpoint_path=${CHECKPOINT_DIR} \
+  --label_file="data/label/fashion_compatibility_prediction.txt" \
+  --feature_file="data/features/test_features.pkl" \
+  --rnn_type="lstm" \
+  --direction="2" \
+  --result_file="fashion_compatibility.pkl"
