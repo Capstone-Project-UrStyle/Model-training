9139d888aa5666f6a8f48cf3bb6cc096f02c40c6
Nguyễn Đặng Tuấn Kiệt <tuankietnk2001@gmail.com>
2023-04-12 11:12:04 +0700

    Update container name

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Pull image command
+# docker build -t python:2.7.12 .
+
 # Run one time only
-# --runtime=nvidia \
 docker run -it \
     -p 8888:8888 \
     -p 6006:6006 \
     -v /home/bunzz/CapstoneProject/model-training/polyvore:/root \
-	tensorflow:0.11
+	python:2.7.12
